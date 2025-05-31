37796d580d5307b667b88f860a5c9261b0c22a77
stasnocap <stasnocap@gmail.com>
2025-05-31 14:44:47 +0300

    add browser and telegram shortcuts

--- config.h
+++ config.h
@@ -104,6 +104,9 @@ static const Key keys[] = {
 	{ 0,			XF86XK_AudioMute,    spawn,       SHCMD("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle") },
 	{ 0,			XF86XK_AudioLowerVolume,    spawn,       SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.05-") },
 	{ 0,			XF86XK_AudioRaiseVolume,    spawn,       SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.05+") },
+
+	{ MODKEY,			XK_w,          spawn,      {.v = (const char*[]){ "qutebrowser", NULL } } },
+	{ MODKEY,			XK_t,          spawn,      {.v = (const char*[]){ "telegram-desktop", NULL } } },
 };
 
 /* button definitions */
